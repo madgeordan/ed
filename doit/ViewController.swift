9c7e1708a1a27e22a4e4f269aa9d20281b971a3d
George Lushch <madgeordan@gmail.com>
2017-06-13 10:30:19 +0300

    not working tableview

--- doit/ViewController.swift
+++ doit/ViewController.swift
@@ -8,12 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        tableView.dataSource = self
+        tableView.delegate = self
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
