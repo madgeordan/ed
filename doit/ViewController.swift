0dd414b14d6acd7403bc85d55fedcfa40d3a5af1
George Lushch <madgeordan@gmail.com>
2017-06-13 10:57:29 +0300

    working tableview with ability yo change cells and their ammount

--- doit/ViewController.swift
+++ doit/ViewController.swift
@@ -12,16 +12,22 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
 
     @IBOutlet weak var tableView: UITableView!
     
+    let emojis = ["😇", "😁", "🤡", "🤠", "😡"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource = self
         tableView.delegate = self
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return emojis.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = emojis[indexPath.row]
+        return cell
+    }
 }
 
